b09670ff88af548911cef22523e25bad69463798
dryan8a@gmail.com <dryan8a@gmail.com>
2021-02-06 15:59:11 -0800

    Add project files.

---
+++ RussianDollProblem/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RussianDollProblem
+{
+    public static class RussianDollEnvelope
+    {
+        public static int EnvelopeCount((int length, int width)[] envelopes)
+        {
+            SortByLength(envelopes);
+            int stackedEnvelopesByLength = CountEnvelopesInOrder(envelopes);
+            
+            SortByWidth(envelopes);
+            int stackedEnvelopesByWidth = CountEnvelopesInOrder(envelopes);
+
+            return stackedEnvelopesByLength > stackedEnvelopesByWidth ? stackedEnvelopesByLength : stackedEnvelopesByWidth;
+        }
+
+        private static int CountEnvelopesInOrder((int length, int width)[] envelopes)
+        {
+            int stackedEnvelopes = 1;
+            int currentTopEnvelope = 0;
+            for (int i = 1; i < envelopes.Length; i++)
+            {
+                if (envelopes[currentTopEnvelope].length > envelopes[i].length && envelopes[currentTopEnvelope].width > envelopes[i].width)
+                {
+                    stackedEnvelopes++;
+                    currentTopEnvelope = i;
+                }
+            }
+            return stackedEnvelopes;
+        }
+
+        private static void SortByLength((int length,int width)[] envelopes)
+        {
+            for(int i = envelopes.Length-1;i>0;i--)
+            {
+                for(int j = 0;j<i;j++)
+                {
+                    if(envelopes[j].length < envelopes[j+1].length)
+                    {
+                        var temp = envelopes[j];
+                        envelopes[j] = envelopes[j + 1];
+                        envelopes[j + 1] = temp;
+                    }
+                }
+            }
+        }
+
+        private static void SortByWidth((int length, int width)[] envelopes)
+        {
+            for (int i = envelopes.Length - 1; i > 0; i--)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (envelopes[j].width < envelopes[j + 1].width)
+                    {
+                        var temp = envelopes[j];
+                        envelopes[j] = envelopes[j + 1];
+                        envelopes[j + 1] = temp;
+                    }
+                }
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ RussianDollTest/UnitTest1.cs
@@ -0,0 +1,41 @@
+using RussianDollProblem;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Xunit;
+
+
+namespace RussianDollTest
+{
+    public class UnitTest1
+    {
+        [Theory]
+        [ClassData(typeof(Data))]
+        public void EnvelopeCountTest(int actualCount, (int, int)[] envelopes)
+        {
+            int resultCount = RussianDollEnvelope.EnvelopeCount(envelopes);
+            Assert.Equal(actualCount, resultCount);
+        }
+    }
+
+    public class Data : IEnumerable<object[]>
+    {
+        private readonly List<object[]> data = new List<object[]>
+        {
+            new object[] { 3, new[] { (50, 50), (25, 50), (20, 18), (15, 5) } },
+            new object[] {2 ,new[] { (50,50),(40,20),(30,30)} },
+            new object[] {2 ,new [] {(60,60),(20,50),(30,10)}},
+            new object[] {4, new[] {(60,60),(50,50),(40,30),(20,20)} }
+        };
+
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            return data.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
